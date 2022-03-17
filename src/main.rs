0858bd9f6b4cd508ca4936605eb1de44ab437b86
Eric <eric@pedr0.net>
2022-03-17 20:51:26 +0100

    Part 1 - Step 7

--- src/main.rs
+++ src/main.rs
@@ -62,14 +62,10 @@ fn drop_ppm_image(file_name: &str, framebuffer: &[u32; WIDTH * HEIGHT]) -> std::
         file.write_all(&[r, g, b])?;
     }
 
-    file.sync_all()?;
-
     Ok(())
 }
 
 fn main() {
-    let mut framebuffer: [u32; WIDTH * HEIGHT] = [pack_color(255, 255, 255, None); WIDTH * HEIGHT];
-
     let map = "0000222222220000\
                1              0\
                1      11111   0\
@@ -88,46 +84,59 @@ fn main() {
                0002222222200000";
     assert!(map.len() == MAP_WIDTH * MAP_HEIGHT);
 
-    let (player_x, player_y, player_a): (f64, f64, f64) = (3.456, 2.345, 1.523);
-    let player_fov = std::f64::consts::PI / 3f64;
+    // Init colors
+    let mut colors = [pack_color(0, 0, 0, None); 10];
+    for i in 0..colors.len() {
+      colors[i] = pack_color(rand::random::<u8>() %255, rand::random::<u8>() % 255, rand::random::<u8>() % 255, None);
+    }
 
-    // Draw the rectangles
-    for (i, c) in map.chars().enumerate() {
+    let (player_x, player_y, mut player_a): (f64, f64, f64) = (3.456, 2.345, 1.523);
+    let player_fov = std::f64::consts::PI / 3f64;
+    for a in 0..360 {
+
+      player_a += 2f64 * std::f64::consts::PI / 360f64;
+
+      let mut framebuffer = [pack_color(255, 255, 255, None); WIDTH * HEIGHT];
+
+      // Draw the map
+      for (i, c) in map.chars().enumerate() {
         let x = i % MAP_WIDTH * RECT_H;
         let y = i / MAP_WIDTH * RECT_W;
+        let color = colors[c as usize % 10];
         match c {
             ' ' => (),
-            _ => draw_rectangle(&mut framebuffer, x, y, RECT_W, RECT_H, pack_color(0, 255, 255, None)),
+            _ => draw_rectangle(&mut framebuffer, x, y, RECT_W, RECT_H, color)
         }
-    }
+      }
 
-    // Draw the player
-    draw_rectangle(&mut framebuffer, (player_x * RECT_W as f64) as usize, (player_y * RECT_H as f64) as usize, 5, 5, pack_color(255, 255, 255, None));
+      // Draw the player
+      draw_rectangle(&mut framebuffer, (player_x * RECT_W as f64) as usize, (player_y * RECT_H as f64) as usize, 5, 5, pack_color(255, 255, 255, None));
 
-    for i in 0..WIDTH / 2 {
-        let angle = player_a - player_fov / 2f64 + player_fov * i as f64 / (WIDTH / 2) as f64;
+      for i in 0..WIDTH / 2 {
+          let angle = player_a - player_fov / 2f64 + player_fov * i as f64 / (WIDTH / 2) as f64;
 
-        // Draw the line
-        for t in 0u32..20000 {
-            let t = f64::from(t) * 0.05;
-            let cx = player_x as f64 + t * angle.cos();
-            let cy = player_y as f64 + t * angle.sin();
+          // Draw the line
+          for t in 0u32..20000 {
+              let t = f64::from(t) * 0.05;
+              let cx = player_x as f64 + t * angle.cos();
+              let cy = player_y as f64 + t * angle.sin();
             
-            let (pix_x, pix_y) = ((cx * RECT_W as f64) as usize, (cy * RECT_H as f64) as usize);
-            framebuffer[pix_x + pix_y * WIDTH] = pack_color(160, 160, 160, None);
+              let (pix_x, pix_y) = ((cx * RECT_W as f64) as usize, (cy * RECT_H as f64) as usize);
+              framebuffer[pix_x + pix_y * WIDTH] = pack_color(160, 160, 160, None);
 
-            match map.chars().nth(cx as usize + cy as usize * MAP_WIDTH) {
-                Some(c) if c != ' ' => {
-                  let column_height = (HEIGHT as f64/ t) as usize;
-                  draw_rectangle(&mut framebuffer, WIDTH / 2 + i, HEIGHT/2 - column_height/2, 1, column_height, pack_color(0, 255, 255, None));
-                  break;
-                },
-                _ => (),
-            }
+              match map.chars().nth(cx as usize + cy as usize * MAP_WIDTH) {
+                  Some(c) if c != ' ' => {
+                    let column_height = (HEIGHT as f64/ t) as usize;
+                    let color = colors[c as usize % 10];
+                    draw_rectangle(&mut framebuffer, WIDTH / 2 + i, HEIGHT/2 - column_height/2, 1, column_height, color);
+                    break;
+                  },
+                  _ => (),
+              }
+          }
+      }
 
-        }
+      // Drop that PPM
+      drop_ppm_image(&format!("./out_{a:0width$}.ppm", width = 3), &framebuffer).expect("Could not write data on disk");
     }
-
-    // Drop that PPM
-    drop_ppm_image("./out.ppm", &framebuffer).expect("Could not write data on disk");
 }
