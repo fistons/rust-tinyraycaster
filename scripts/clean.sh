6e4be2a65c8e367d5ccae0b6d92e9ad58516161c
Eric <eric@pedr0.net>
2022-04-29 23:51:50 +0200

    Delete useless script

--- scripts/clean.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rm *.ppm
-rm *.mp4
--- scripts/make-a-gif.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm *.ppm
-rm *.mp4
-
-time cargo run --release
-time ffmpeg -framerate 60 -i out_%03d.ppm -c:v libx264 -crf 25 -vf "format=yuv420p" -movflags +faststart wulgenstein.mp4
